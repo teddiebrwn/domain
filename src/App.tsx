25dddd6a67614aff75c9b763ebcfca6d1cd14244
Tranvu <huynhdong1115@gmail.com>
2025-04-11 16:16:09 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-[#f1f1f1] flex items-center justify-center">
+      <h1 className="text-4xl tracking-wide text-[#1E293B]">Love Jinnie.</h1>
+    </div>
+  );
+}
+export default App;

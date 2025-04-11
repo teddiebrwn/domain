1243a59e293b2aea8609c09a07eb6d1918f6e521
Tranvu <huynhdong1115@gmail.com>
2025-04-11 16:40:55 +0700

    ✨

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-[#f1f1f1] flex items-center justify-center">
-      <h1 className="text-4xl tracking-wide text-[#1E293B]">Love Jinnie.</h1>
+    <div className="w-screen h-screen bg-[#1a1a1a] flex items-center justify-center">
+      <div className="bg-black rounded-3xl shadow-xl px-10 py-8 text-center">
+        <h1 className="text-4xl md:text-5xl font-semibold text-white tracking-tight">
+          teddiexjinne<span className="text-blue-400">.</span>
+        </h1>
+      </div>
     </div>
   );
 }
+
 export default App;

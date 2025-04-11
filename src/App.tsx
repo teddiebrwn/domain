6c674de29b07cef3174605a8ba21b497f929224b
Tranvu <huynhdong1115@gmail.com>
2025-04-11 17:10:20 +0700

    ✨

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-[#1a1a1a] flex items-center justify-center">
-      <div
-        className="
-          bg-black rounded-3xl shadow-2xl text-center
-          px-6 py-4
-          sm:px-10 sm:py-6
-          md:px-16 md:py-8
-          lg:px-24 lg:py-10
-        "
-      >
-        <h1
-          className="
-            font-bold tracking-tight text-white
-            text-[24px]
-            sm:text-[40px]
-            md:text-[56px]
-            lg:text-[72px]
-            leading-tight
-          "
-        >
-          teddiexjinne<span className="text-blue-400">.</span>
-        </h1>
-      </div>
+    <div className="h-screen w-screen flex items-center justify-center">
+      Cooking.
     </div>
   );
 }
